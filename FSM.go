a710120fa38b65d421da57eac560ab34826e5964
Andreadi Nikolai <NikolayAndreadi@gmail.com>
2022-10-27 16:35:43 +0400

    refactor, db as reference, better error handling

--- FSM.go
+++ FSM.go
@@ -1,5 +1,4 @@
-// Finite State Machine implementation
-
+// FSM - Finite State Machine implementation
 package main
 
 import (
@@ -9,6 +8,7 @@ import (
 	"sync"
 
 	"github.com/jackc/pgx/v5"
+	"github.com/jackc/pgx/v5/pgxpool"
 	tele "gopkg.in/telebot.v3"
 )
 
@@ -20,12 +20,13 @@ const (
 
 // StateSetterType is a function that validates, checks and switches states
 // if nextState equals ResetState, then state of the machine will be terminated (for concrete UserID)
-// if nextState equals ResumeState, then state of the machine will not be changed
+// if nextState equals ResumeState or NoState, then state of the machine will not be changed
 type StateSetterType func(c tele.Context) (nextState string, err error)
 
-// State is a struct describing State logic.
-// TextOnTrigger will be shown to user right after State is triggered
-// Corresponding StateSetter would be triggered on FSM.UpdateState method
+// State is a struct describing concrete FSM element.
+// OnTrigger contains the action that will be executed on stage trigger. Can be string or telebot.Sendable
+// ExtraOnTrigger contains additional events like menu with buttons. Can be string or telebot.Sendable
+// StateSetter will be triggered on FSM.UpdateState and will decide if state changes.
 type State struct {
 	OnTrigger      interface{}
 	ExtraOnTrigger []interface{}
@@ -34,15 +35,17 @@ type State struct {
 }
 
 // FSM is a finite state machine structure
-// All states are stored in a concurrent-ready statePool map, where key is a unique StateName
+// All states are stored in the statePool map, where key is a unique StateName
 type FSM struct {
+	db        *pgxpool.Pool
 	statePool map[string]State // key - StateName
 	mu        *sync.RWMutex
 }
 
 // NewFSM is a constructor of the FSM struct
-func NewFSM() FSM {
+func NewFSM(db *pgxpool.Pool) FSM {
 	return FSM{
+		db:        db,
 		statePool: make(map[string]State),
 		mu:        &sync.RWMutex{},
 	}
@@ -57,17 +60,15 @@ func (s *FSM) containsStateName(stateName string) bool {
 }
 
 // AddState method adds a new State to the FSM with unique stateName.
-// textOnTrigger will be shown to user right after State is triggered
-// Corresponding stateSetter would be triggered on FSM.UpdateState method
-func (s *FSM) AddState(stateName string, OnTrigger interface{}, stateSetter StateSetterType, ExtraOnTrigger ...interface{}) {
+func (s *FSM) AddState(stateName string, onTrigger interface{}, stateSetter StateSetterType, extraOnTrigger ...interface{}) {
 	// check uniqueness of the stateName
 	if s.containsStateName(stateName) {
 		panic(fmt.Errorf("AddState: State %s already exists", stateName))
 	}
 
 	state := State{
-		OnTrigger:      OnTrigger,
-		ExtraOnTrigger: ExtraOnTrigger,
+		OnTrigger:      onTrigger,
+		ExtraOnTrigger: extraOnTrigger,
 		StateSetter:    stateSetter,
 	}
 	s.mu.Lock()
@@ -75,25 +76,41 @@ func (s *FSM) AddState(stateName string, OnTrigger interface{}, stateSetter Stat
 	s.statePool[stateName] = state
 }
 
+// ResetState clears out saved state in the database
+func (s *FSM) ResetState(c tele.Context) error {
+	err := s.ClearStateVars(c)
+	if err != nil {
+		return fmt.Errorf("ResetState: %w", err)
+	}
+
+	err = s.setStateToDB(c, "")
+	if err != nil {
+		return fmt.Errorf("ResetState: %w", err)
+	}
+
+	return nil
+}
+
 // TriggerState is a starting point of the state
 func (s *FSM) TriggerState(c tele.Context, stateName string) error {
-	if stateName == ResumeState {
-		return nil
-	}
 	if stateName == ResetState {
-		return s.ResetState(c)
+		err := s.ResetState(c)
+		if err != nil {
+			return fmt.Errorf("TriggerState: %w", err)
+		}
 	}
+
 	// validate stateName
 	if !s.containsStateName(stateName) {
-		return fmt.Errorf("TriggerState: Unknown state '%s'", stateName)
+		return fmt.Errorf("TriggerState[%d]: unknown state '%s'", c.Sender().ID, stateName)
 	}
 
 	// Set a new state to the database
-	if err2 := setStateToDB(c, stateName); err2 != nil {
-		return err2
+	if err := s.setStateToDB(c, stateName); err != nil {
+		return fmt.Errorf("TriggerState: %w", err)
 	}
 
-	// Show send TextOnTrigger message to the user
+	// exec OnTrigger for the user
 	s.mu.RLock()
 	defer s.mu.RUnlock()
 	if state, ok := s.statePool[stateName]; ok {
@@ -103,20 +120,11 @@ func (s *FSM) TriggerState(c tele.Context, stateName string) error {
 	}
 }
 
-// ResetState clears out saved state in the database
-func (s *FSM) ResetState(c tele.Context) error {
-	err := ClearStateVars(c)
-	if err != nil {
-		fmt.Println("ResetState: %w", err)
-	}
-
-	return setStateToDB(c, "")
-}
-
+// UpdateState is a method that should be executed at OnText, OnImage etc.. handlers
 func (s *FSM) UpdateState(c tele.Context) error {
-	stateName, err := getStateFromDB(c)
+	stateName, err := s.getStateFromDB(c)
 	if err != nil {
-		return err
+		return fmt.Errorf("UpdateState: %w", err)
 	}
 	if stateName == NoState {
 		return c.Reply("Не могу ответить на это сообщение =(")
@@ -126,107 +134,137 @@ func (s *FSM) UpdateState(c tele.Context) error {
 	state, ok := s.statePool[stateName]
 	s.mu.RUnlock()
 	if !ok {
-		return fmt.Errorf("UpdateState: Unknown state '%s'", stateName)
+		return fmt.Errorf("UpdateState[%d]: Unknown state '%s'", c.Sender().ID, stateName)
 	}
 
 	newState, err := state.StateSetter(c)
 	if err != nil {
-		return err
-	}
-	if newState == ResetState {
-		return s.ResetState(c)
+		return fmt.Errorf("UpdateState: %w", err)
 	}
 
 	if (newState != ResumeState) || (newState != stateName) {
 		err = s.TriggerState(c, newState)
 		if err != nil {
-			return err
+			return fmt.Errorf("UpdateState: %w", err)
 		}
 	}
 
 	return nil
 }
 
-// STATE-RELATED VARIABLE METHOD
+// STATE-VARIABLE-RELATED METHODS
 
 // SetStateVar saves jsonb field for user
-func SetStateVar(c tele.Context, varName string, value string) error {
+func (s *FSM) SetStateVar(c tele.Context, varName string, value string) error {
 	userID := c.Sender().ID
-	_, err := DB.Exec(context.Background(), `
+
+	_, err := s.db.Exec(context.Background(), `
 		UPDATE states
 		SET temp_vars =  temp_vars || jsonb_build_object($1::text,$2::text)
 		WHERE user_id = $3
 	`, varName, value, userID)
-	return err
+
+	if err != nil {
+		return fmt.Errorf("SetStateVar[%d], varName %s, value %s: postgres QueryRow error %w",
+			userID, varName, value, err)
+	}
+
+	return nil
 }
 
 // GetStateVar extracts variable from jsonb column of table 'states'.
-// if exists, ok param will be true
-func GetStateVar(c tele.Context, varName string) (value string, ok bool, err error) {
+// if exists, ok return value will be true
+func (s *FSM) GetStateVar(c tele.Context, varName string) (value string, ok bool, err error) {
 	userID := c.Sender().ID
-	err = DB.QueryRow(context.Background(), `
+
+	err = s.db.QueryRow(context.Background(), `
 		SELECT temp_vars->>$1 FROM states
 		WHERE user_id = $2
 		`, varName, userID).Scan(&value)
-	ok = true
 
-	if err != nil {
-		ok = false
-	}
+	ok = err == nil
+
 	if err == pgx.ErrNoRows {
 		err = nil
 	}
+	if err != nil {
+		return value, ok, fmt.Errorf("GetStateVar[%d], varName %s: postgres QueryRow error %w", userID, varName, err)
+	}
+
 	return
 }
 
-func GetStateVars(c tele.Context) (values map[string]string, err error) {
+// GetStateVars extracts whole jsonb column to map
+func (s *FSM) GetStateVars(c tele.Context) (values map[string]string, err error) {
+	var strJSON []byte
 	userID := c.Sender().ID
 
-	var strJSON []byte
-	err = DB.QueryRow(context.Background(), `
+	err = s.db.QueryRow(context.Background(), `
 		SELECT temp_vars FROM states
 		WHERE user_id = $1
 		`, userID).Scan(&strJSON)
-
 	if err == pgx.ErrNoRows {
 		return nil, nil
 	}
+	if err != nil {
+		return nil, fmt.Errorf("GetStateVars[%d]: postgres QueryRow error %w", userID, err)
+	}
+
 	err = json.Unmarshal(strJSON, &values)
+	if err != nil {
+		return nil, fmt.Errorf("GetStateVars[%d]: json.Unmarshal error %w", userID, err)
+	}
+
 	return
 }
 
 // ClearStateVars flushes whole jsonb field for user
-func ClearStateVars(c tele.Context) error {
+func (s *FSM) ClearStateVars(c tele.Context) error {
 	userID := c.Sender().ID
-	_, err := DB.Exec(context.Background(), `
+
+	_, err := s.db.Exec(context.Background(), `
 		UPDATE states
 		SET temp_vars = '{}'::jsonb
 		WHERE user_id = $1
 	`, userID)
-	return err
+	if err != nil {
+		return fmt.Errorf("ClearStateVars[%d]: postgres exec error %w", userID, err)
+	}
+
+	return nil
 }
 
-// DATABASE functions
+// DATABASE METHODS
 
-func setStateToDB(c tele.Context, stateName string) error {
+func (s *FSM) setStateToDB(c tele.Context, stateName string) error {
 	userID := c.Sender().ID
-	_, err := DB.Exec(context.Background(), `
+
+	_, err := s.db.Exec(context.Background(), `
 		INSERT INTO states (user_id, state) 
 		VALUES($1, $2)
 		ON CONFLICT (user_id) DO UPDATE 
 			SET state = excluded.state 
 		`, userID, stateName)
-	return err
+	if err != nil {
+		return fmt.Errorf("setStateToDB[%d], stateName %s: postgres exec error %w", userID, stateName, err)
+	}
+
+	return nil
 }
 
-func getStateFromDB(c tele.Context) (stateName string, err error) {
+func (s *FSM) getStateFromDB(c tele.Context) (stateName string, err error) {
 	userID := c.Sender().ID
-	err = DB.QueryRow(context.Background(),
+
+	err = s.db.QueryRow(context.Background(),
 		"SELECT state FROM states WHERE user_id = $1", userID).Scan(&stateName)
 	if err == pgx.ErrNoRows {
 		// no state -> do nothing
 		stateName = NoState
 		err = nil
 	}
+	if err != nil {
+		return stateName, fmt.Errorf("getStateFromDB[%d]: postgres QueryRow error %w", userID, err)
+	}
+
 	return stateName, err
 }
